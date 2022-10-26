0e01a66f0ef92436837fb6c0b317a9ff95a453e7
ST4R20RD <goncalo.estrelado@hotmail.com>
2022-10-26 18:04:39 +0100

    Added PrivateRoute component to redirect User to Signup/Login when there's no user logged in

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Layout } from "./components";
+import { PrivateRoute } from "./components/PrivateRoute";
 import { Feed, Groups, Profile, InsideGroup, SignupLogin } from "./pages";
 
 function App() {
@@ -11,7 +12,14 @@ function App() {
         <Route index element={<Feed />} />
         <Route path="/Groups" element={<Groups />} />
         <Route path="/Groups/:id" element={<InsideGroup />} />
-        <Route path="/Profile" element={<Profile />} />
+        <Route
+          path="/Profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
         <Route path="/Signup-Login" element={<SignupLogin />} />
       </Route>
     </Routes>
