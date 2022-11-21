1785a43265fb54fa3f6ff8abe392cfa74fa248f8
ST4R20RD <goncalo.estrelado@hotmail.com>
2022-11-21 01:36:32 +0000

    improved getCurrentDate

--- src/components/CardForm/getCurrentDate.tsx
+++ src/components/CardForm/getCurrentDate.tsx
@@ -1,12 +1,14 @@
+function padTo2Digits(num: number) {
+  return String(num).padStart(2, "0");
+}
+
 export function getCurrentDate(separator = "-") {
   let newDate = new Date();
-  let hour = newDate.getHours();
-  let minutes = newDate.getMinutes();
-  let date = newDate.getDate();
-  let month = newDate.getMonth() + 1;
-  let year = newDate.getFullYear();
+  let hour = padTo2Digits(newDate.getHours());
+  let minutes = padTo2Digits(newDate.getMinutes());
+  let date = padTo2Digits(newDate.getDate());
+  let month = padTo2Digits(newDate.getMonth() + 1);
+  let year = padTo2Digits(newDate.getFullYear());
 
-  return `${hour}:${minutes < 10 ? `0${minutes}` : `${minutes}`} ${year}${separator}${
-    month < 10 ? `0${month}` : `${month}`
-  }${separator}${date}`;
+  return `${hour}:${minutes} ${year}${separator}${month}${separator}${date}`;
 }
