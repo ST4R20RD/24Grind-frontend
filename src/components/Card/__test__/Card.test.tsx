ced1d40f745cc6827d76ae59e018d4aaeed52e82
ST4R20RD <goncalo.estrelado@hotmail.com>
2022-12-08 20:37:23 +0000

    fixed Card tests

--- src/components/Card/__test__/Card.test.tsx
+++ src/components/Card/__test__/Card.test.tsx
@@ -18,7 +18,8 @@ const MockCard = {
   day: "05/09/2022",
   location: "Quinta da Alagoa",
   category: "Phisical Training",
-  image: "https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg?w=2000",
+  image:
+    "https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg?w=2000",
   description: "100 💪, 30 Abdominais, 20 Dorsais",
   participants: [],
 };
@@ -26,14 +27,6 @@ const MockCard = {
 afterAll(() => {
   nock.cleanAll();
   nock.restore();
-});
-
-describe("Card Tests on Loading", () => {
-  it("should render Spinner on Loading", () => {
-    render(<Card {...MockCard} />);
-    const divElement = screen.getByTestId("card-spinner");
-    expect(divElement).toBeInTheDocument();
-  });
 });
 
 beforeEach(() => {
@@ -67,15 +60,15 @@ describe("Card Tests", () => {
     it("should render remainder number indicator when participants are more than 5", async () => {
       MockCard.participants.length = 6;
       render(<Card {...MockCard} />);
-      const paragraphElement = await screen.findByTestId("participants-crowded");
+      const paragraphElement = await screen.findByTestId(
+        "participants-crowded"
+      );
       expect(paragraphElement).toBeInTheDocument();
     });
 
     it("should not render remainder number indicator when participants are less than 5", async () => {
       MockCard.participants.length = 4;
       render(<Card {...MockCard} />);
-      const divElement = await screen.findByTestId("success-section");
-      await waitFor(() => expect(divElement).toBeInTheDocument());
       const paragraphElement = screen.queryByTestId("participants-crowded");
       expect(paragraphElement).not.toBeInTheDocument();
     });
