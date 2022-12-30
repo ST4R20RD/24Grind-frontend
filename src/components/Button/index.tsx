1c2455e23baed2285be8c81439b4fbb7de5ddfa5
ST4R20RD <goncalo.estrelado@hotmail.com>
2022-12-30 04:06:39 +0000

    Added option to pass more className properties to Button Comp

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export function Button({ children, submit, onClick }: any) {
+export function Button({ children, submit, onClick, className }: any) {
   return (
     <button
       type={submit ? `submit` : `button`}
-      className=" text-white bg-lustRed dark:text-black font-semibold rounded-lg px-3 mx-3 py-1"
+      className={`text-white bg-lustRed dark:text-black font-semibold rounded-lg px-3 mx-3 py-1 ${className}`}
       onClick={onClick}
     >
       {children}
