08a6ea0735de630bf6359025adfb4662cb7df509
ST4R20RD <goncalo.estrelado@hotmail.com>
2022-12-30 02:10:37 +0000

    added new color extensions for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{ts,tsx}",
-  ],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        chinBlackDark: "#111111",
+        chinBlack: "#161616",
+        eerieBlack: "#1C1C1C",
+        lustRed: "#E31C25",
+        ueRed: "#BB000E",
+      },
+    },
   },
   plugins: [],
-}
+};
