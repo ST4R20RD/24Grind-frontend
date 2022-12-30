0706dd5fa4bdcc2e4bae3670d105c4b0785aeba7
ST4R20RD <goncalo.estrelado@hotmail.com>
2022-12-30 19:33:26 +0000

    switched font color between light/dark modes on Button Component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,7 @@ export function Button({ children, submit, onClick, className }: any) {
   return (
     <button
       type={submit ? `submit` : `button`}
-      className={`text-white bg-lustRed dark:text-black font-semibold rounded-lg px-3 mx-3 py-1 ${className}`}
+      className={`text-black bg-lustRed dark:text-white font-semibold rounded-lg px-3 mx-3 py-1 ${className}`}
       onClick={onClick}
     >
       {children}
