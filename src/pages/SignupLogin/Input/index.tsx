b7ad960162039d4f27f0401c572f850eaac95a9d
ST4R20RD <goncalo.estrelado@hotmail.com>
2022-12-30 02:14:08 +0000

    REFACTOR Input Component

---
+++ src/pages/SignupLogin/Input/index.tsx
@@ -0,0 +1,7 @@
+export function Input({ children }: any) {
+  return (
+    <div className="flex flex-col justify-between p-1 my-1 text-black dark:text-white">
+      {children}
+    </div>
+  );
+}
