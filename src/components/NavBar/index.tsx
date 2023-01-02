8ffbd794ed9c0f7c6604e554d728e8245fdb3194
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-01-02 17:45:13 +0000

    ADD labels to Navbar Icons

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -5,26 +5,25 @@ import { User } from "../../utils/types";
 import { useEffect } from "react";
 import { useGetUser } from "../../lib/api-hooks";
 
-const iconClassname = "flex items-center justify-center h-8 w-8 text-3xl m-1";
+const iconClassname = "flex items-center justify-center h-6 w-6 text-2xl";
 
 const linkDivClassname = "flex flex-col items-center";
 
 export function NavBar() {
-  const CurrentUser = JSON.parse(
-    localStorage.getItem("currentUser") as string
-  ) as User;
+  const CurrentUser = JSON.parse(localStorage.getItem("currentUser") as string) as User;
 
-  const [user, userFetchState, getUser] = useGetUser();
+  const [, , getUser] = useGetUser();
 
   useEffect(() => {
     if (!CurrentUser) return;
     getUser(CurrentUser.id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div
       data-testid="navbar"
-      className="h-10 bg-white border-t-[1px] border-zinc-700 dark:bg-eerieBlack "
+      className="h-12 bg-white border-t-[1px] border-zinc-800 dark:bg-eerieBlack p-1"
     >
       <div>
         <div className="flex justify-around items-center dark:text-white">
@@ -32,6 +31,7 @@ export function NavBar() {
             <Link to={"/"} className={iconClassname} data-testid="feedLink">
               <TiHome />
             </Link>
+            <h4>Home</h4>
           </div>
           <div className={linkDivClassname} data-testid="profileLink">
             <Link
@@ -40,6 +40,7 @@ export function NavBar() {
             >
               <IoMdPerson />
             </Link>
+            <h4>Profile</h4>
           </div>
         </div>
       </div>
